33da5e85fd82f5e5e3ee5dcf3f1f231acd0917d5
Chris <thatoneguy43@users.noreply.github.com>
2018-06-04 01:31:08 -0500

    Cleaned up code
    I was kind of in a rush to get this program out so now I have cleaned up the code. Made it a little faster and looks a little nicer.

--- Retinoblastoma_Detector(face).py
+++ Retinoblastoma_Detector(face).py
@@ -2,15 +2,16 @@ import numpy as np
 import cv2
 import os
 from sklearn import svm
+import pickle
 
 w = 50
 h = 100
 c = 3
 
 X_data = np.zeros((100, w * h * c), dtype=np.uint8)
-Test_data = np.zeros((12, w * h * c), dtype=np.uint8) #change the first value if you have a different amount of pictures
 Y_data = np.zeros((100,), dtype=np.uint8)
 eye = np.zeros((1, w * h * c), dtype=np.uint8)
+
 
 with open('filename.txt', 'r') as f:
     index = 0
@@ -20,13 +21,11 @@ with open('filename.txt', 'r') as f:
         Y_data[index] = int(c)
         img = cv2.imread(os.path.join("Training",p))
         print os.path.join("Training",p)
-        cv2.imshow('Training before resize', img)
         nimg = cv2.resize(img,(h,w))
-        cv2.imshow('Training after resize', nimg)
         X_data[index] = nimg.flatten()
         index += 1
 
-#start of svm, change C value to get best result
+#start of svm, changes C value to get best result
 best = 0
 bestc = 1
 for i in xrange(0, 7):
@@ -51,38 +50,33 @@ eye_cascade = cv2.CascadeClassifier("cascade/haarcascade_eye.xml")
 
 #add picture here
 img = cv2.imread('image.jpg')
-
 while(True):
-	open = True
-	if open == True:
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x,y,w,h) in faces:
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = img[y:y+h, x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_gray)
-            for (ex,ey,ew,eh) in eyes:
-                print ex,ey,ew,eh
-                neye = roi_color[ey:ey+eh,ex:ex+ew]
-                #neye = cv2.resize(neye,((ey+eh)/2,(ex+ew)/2), interpolation = cv2.INTER_CUBIC) #I'm trying to make this zoom in on the eye
-                cv2.imshow('eye',neye)
-                cv2.waitKey(1000)
-                neye = neye.flatten()
-                eye[0] = neye.shape[0]
-                prediction = bestsvm.predict(eye.reshape(1,-1))
-                print prediction
-                if int(prediction) == 0:
-                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)#No Retinoblastoma, just show its detecting a normal eye
-                else:
-                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)#there is Retinoblastoma, create a red square around the eye
-        cv2.imshow('image',roi_color)
-        cv2.waitKey(10000) #wait time after the boxes are made around the eyes
-        
-        key = cv2.waitKey(1)
-        if (key==27):
-            break
-	else:
-		break
-
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            neye = roi_color[ey:ey+eh,ex:ex+ew]
+            cv2.imshow('eye',neye)
+            cv2.waitKey(1000)
+            neye = neye.flatten()
+            eye[0] = neye.shape[0]
+            prediction = bestsvm.predict(eye.reshape(1,-1))
+            print "prediction", prediction
+            if int(prediction) == 0:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)#No Retinoblastoma, just show its detecting a normal eye
+            else:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)#there is Retinoblastoma, create a red square around the eye
+    cv2.putText(roi_color,"If there is a red box around ",(0,200), 1, 1,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(roi_color,"an eye, double check if it ",(0,220), 1, 1,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(roi_color,"has retinoblastoma",(0,240), 1, 1,(255,255,255),2,cv2.LINE_AA)
+    cv2.imshow("image", roi_color)
+    example = cv2.imread('Retinoblastoma_example_image.jpg')
+    cv2.imshow("Retinoblastoma example", example)
+    cv2.waitKey(10000) #wait time after the boxes are made around the eyes
+    break
+print "Thanks for running my program, I hope it helped!"
 cv2.destroyAllWindows()
